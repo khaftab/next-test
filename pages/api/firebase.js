a2577b21093c7fd93cf0add419aea8891575fa16
khaftab <kh7aftab8uddin6ahmed@gmail.com>
2021-05-19 21:35:45 +0530

    fix file import bug

--- pages/api/firebase.js
+++ pages/api/firebase.js
@@ -1,24 +1,24 @@
-const serviceAccount = require('../../serviceAccountKey.json')
-const admin = require('firebase-admin')
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://net-ninja-part--2.firebaseio.com"
-});
-
-export default (req, res) => {
-    const  email  = 'ssg@ssg.com'
-    return admin.auth().getUserByEmail(email).then(user => {
-        return admin.auth().setCustomUserClaims(user.uid, {
-            admin: true
-        })
-    }).then(() => {
-
-        return res.status(200).json({ messege: `Congo you ${email} are now a admin` })
-
-    }).catch((err) => res.status(400).json({ message: err }))
-}
+// const serviceAccount = require('../../serviceAccountKey.json')
+// const admin = require('firebase-admin')
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     databaseURL: "https://net-ninja-part--2.firebaseio.com"
+// });
 
 // export default (req, res) => {
-//     console.log(req.body)
-//     res.status(200).json({message: 'success'})
+//     const  email  = 'ssg@ssg.com'
+//     return admin.auth().getUserByEmail(email).then(user => {
+//         return admin.auth().setCustomUserClaims(user.uid, {
+//             admin: true
+//         })
+//     }).then(() => {
+
+//         return res.status(200).json({ messege: `Congo you ${email} are now a admin` })
+
+//     }).catch((err) => res.status(400).json({ message: err }))
 // }
+
+// // export default (req, res) => {
+// //     console.log(req.body)
+// //     res.status(200).json({message: 'success'})
+// // }
